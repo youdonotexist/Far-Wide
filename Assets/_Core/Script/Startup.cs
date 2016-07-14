d3572fb134c25d70ec822ffbfb4ef0e51d030c25
Mike Daly <mdaly@l4digital.com>
2016-07-14 00:18:00 -0700

    Initial Commit.

---
+++ Assets/_Core/Script/Startup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gamelogic.Grids;
+
+public class Startup : MonoBehaviour {
+
+    [SerializeField]
+    private FarAndWide.FWGrid fwGrid;
+
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    [SerializeField]
+    private GameObject headPrefab;
+
+	// Use this for initialization
+	void Start () {
+        SpriteCell[] corners = fwGrid.getCorners();
+
+        GameObject.Instantiate(playerPrefab, corners[0].Center, Quaternion.identity);
+        GameObject.Instantiate(playerPrefab, corners[1].Center, Quaternion.identity);
+        GameObject.Instantiate(headPrefab, corners[2].Center, Quaternion.identity);
+        GameObject.Instantiate(headPrefab, corners[3].Center, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
